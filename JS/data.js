67514004e7c77d4c7d3c9f279ab0ec54fdcd70fc
Arber Islamaj <arber-islamaj@hotmail.com>
2023-10-07 15:50:30 +0200

    first commit

---
+++ JS/data.js
@@ -0,0 +1,58 @@
+let tasks =`[
+{
+"day": "Monday",
+"taskName": "Shopping", 
+"image": "images/monday.jpg", 
+"description": "Shop for weekly groceries.", 
+"duration": "1 hour",
+"location": "Spar",
+"importance": 0  
+},{
+"day": "Tuesday",
+"taskName": "Training", 
+"image": "images/tuesday.jpg", 
+"description": "Going to the fitness studio.", 
+"duration": "2 hours",
+"location": "Gym",
+"importance": 0  
+},{
+"day": "Wednesday",
+"taskName": "Reading", 
+"image": "images/wednesday.jpg", 
+"description": "Reading books.", 
+"duration": "1 hour",
+"location": "Home",
+"importance": 0  
+},{
+"day": "Thursday",
+"taskName": "Training", 
+"image": "images/thursday.jpg", 
+"description": "Bike ride for 20km.", 
+"duration": "3 hours",
+"location": "Donau",
+"importance": 0  
+},{
+"day": "Friday",
+"taskName": "Cleaning", 
+"image": "images/friday.jpg", 
+"description": "Cleaning the house.", 
+"duration": "2 hours",
+"location": "Home",
+"importance": 0  
+},{
+"day": "Saturday",
+"taskName": "Training", 
+"image": "images/saturday.jpg", 
+"description": "Running in fresh air.", 
+"duration": "2 hours",
+"location": "Prater",
+"importance": 0
+},{
+"day": "Sunday",
+"taskName": "Relaxing", 
+"image": "images/sunday.jpg", 
+"description": "Going outside the city for a picnic.", 
+"duration": "6 hours",
+"location": "Lower Austria",
+"importance": 0 
+}]`
---
+++ JS/main.js
@@ -0,0 +1,126 @@
+
+// let week = JSON.parse(tasks);
+
+// week.forEach(show => {
+//     document.getElementById("result").innerHTML += 
+//     `<div class="card">
+//         <img src="${show.image}" class="card-img-top" alt="${show.taskName}">
+//         <div class="card-body">
+//             <h5 class="card-title">${show.day}</h5>
+//             <p class="card-text">${show.taskName} day</p>
+//             <p class="card-text">${show.description}</p>
+//             <p class="card-text">Duration: ${show.duration}</p>
+//             <p class="card-text">Location: ${show.location}</p>
+//             <button class="btn btn-primary likeBtn">Importance: 
+//             <span class="import">${show.importance}</span>
+//             </button>
+//         </div>
+//     </div>`;
+// });
+
+// let buttons = document.querySelectorAll(".likeBtn");
+
+// buttons.forEach((single,i) => {
+//     single.addEventListener("click", function () {
+//         incImp(i);
+//     })
+// });
+
+// const filteredTasks = week.filter(task => task.importance < 6);
+
+
+// function incImp(iBtn) {
+//     week[iBtn].importance++;
+// document.querySelectorAll(".import")[iBtn].innerText = week[iBtn].importance
+
+// }
+
+// document.getElementById("smt").addEventListener("click", function(){
+//     let newFunc = week.sort((a, b) => b.importance - a.importance);
+//     document.getElementById("result").innerHTML = "";
+//     newFunc.forEach(show => {
+//         document.getElementById("result").innerHTML += 
+//         `<div class="card">
+//             <img src="${show.image}" class="card-img-top" alt="${show.taskName}">
+//             <div class="card-body">
+//                 <h5 class="card-title">${show.day}</h5>
+//                 <p class="card-text">${show.taskName} day</p>
+//                 <p class="card-text">${show.description}</p>
+//                 <p class="card-text">Duration: ${show.duration}</p>
+//                 <p class="card-text">Location: ${show.location}</p>
+//                 <button class="btn btn-primary likeBtn">Importance: 
+//                 <span class="import">${show.importance}</span>
+//                 </button>
+//             </div>
+//         </div>`;
+//     });
+// })
+
+
+// I was not able to limit increasing the "importance" till 5 by clicking, 
+// thats why I created a new code using renderTasks
+
+
+let week = JSON.parse(tasks);
+
+function getColorByImportance(importance) {
+    if (importance === 0 || importance === 1) {
+        return "green";
+    } else if (importance === 2 || importance === 3) {
+        return "yellow";
+    } else {
+        return "red";
+    }
+}
+
+function renderTasks() {
+    const resultElement = document.getElementById("result");
+    resultElement.innerHTML = "";
+
+    week.forEach((show, i) => {
+        const color = getColorByImportance(show.importance);
+        resultElement.innerHTML +=
+            `<div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+                <img src="${show.image}" class="card-img-top" 
+                alt="${show.taskName}">
+                <div class="card-body " style="background-color: ${color}">
+                    <h5 class="card-title">${show.day}</h5>
+                    <p class="card-text">${show.taskName} day</p>
+                    <p class="card-text">${show.description}</p>
+                    <p class="card-text">Duration: ${show.duration}</p>
+                    <p class="card-text">Location: ${show.location}</p>
+                    <button class="btn btn-primary likeBtn">Importance: 
+                        <span class="import">${show.importance}</span>
+                    </button>
+                </div>
+            </div>`;
+    });
+
+    const buttons = document.querySelectorAll(".likeBtn");
+
+    buttons.forEach((single, i) => {
+        single.addEventListener("click", function () {
+            incImp(i);
+        });
+    });
+}
+
+function incImp(iBtn) {
+    if (week[iBtn].importance < 5) {
+        week[iBtn].importance++;
+        renderTasks();
+    }
+}
+
+document.getElementById("smt").addEventListener("click", function () {
+    week.sort((a, b) => a.importance - b.importance);
+    renderTasks();
+});
+
+renderTasks();
+
+
+
+
+
+  
